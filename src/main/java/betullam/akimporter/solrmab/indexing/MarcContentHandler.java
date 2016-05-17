13822aae743af917ca30843f0890b40bee9e0eab
Michael Birkner <michael.birkner@akwien.at>
2016-05-17 07:31:54 +0200

    Removed unnecessary import statement.

--- src/main/java/betullam/akimporter/solrmab/indexing/MarcContentHandler.java
+++ src/main/java/betullam/akimporter/solrmab/indexing/MarcContentHandler.java
@@ -34,7 +34,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
