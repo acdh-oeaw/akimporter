f4387cefc1ce43aa3e46fe53ace00701d1e9ca66
Michael Birkner <michael.birkner@akwien.at>
2016-11-02 16:10:49 +0100

    Started parsing info from Mets/Mods file.

--- src/main/java/betullam/akimporter/solrmab/indexing/MetsContentHandler.java
+++ src/main/java/betullam/akimporter/solrmab/indexing/MetsContentHandler.java
@@ -24,7 +24,10 @@
  */
 package main.java.betullam.akimporter.solrmab.indexing;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.xml.sax.Attributes;
@@ -32,59 +35,539 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 
+import main.java.betullam.akimporter.solrmab.indexing.MetsRawRecord.DmdSec;
+import main.java.betullam.akimporter.solrmab.indexing.MetsRawRecord.Participant;
+
 public class MetsContentHandler implements ContentHandler {
 
 	private SolrServer solrServer;
 	private String elementContent;
 	private String timeStamp;
 	private boolean print = true;
+
+	// XML sections we need to process
+	private boolean isRecord = false;
+	private boolean isDmdSec = false;
+	private boolean isLogicalStructMap = false;
+	private boolean isPhysicalStructMap = false;
+	private boolean isStructLink = false;
+	private boolean isClassification = false;
+	private boolean isOriginInfo = false;
+	private boolean isPublisher = false;
+	private boolean isPlace = false;
+	private boolean isYear = false;
+	private boolean isTitleInfo = false;
+	private boolean isSubtitle = false;
+	private boolean isTitle = false;
+	private boolean isRelatedItem = false;
+	private boolean isReview = false;
+	private boolean isReviewedBookTitle = false;
+	private boolean isRecordInfo = false;
+	private boolean isRecordIdentifier = false;
+	private boolean isAcNo = false;
+	private boolean isAkIdentifier = false;
+	private boolean isPart = false;
+	private boolean isIssue = false;
+	private boolean isVolume = false;
+	private boolean isNumber = false;
+	private boolean isLanguageTerm = false;
+	private boolean isAbstract = false;
+	private boolean isName = false;
+	private boolean isPersonalName = false;
+	private boolean isCorporateName = false;
+	private boolean isRoleTerm = false;
+	private boolean isFamilyNamePart = false;
+	private boolean isGivenNamePart = false;
+	private boolean isDiv_logical = false;
+	private boolean isDiv_physical = false;
+
+	// Attribute values that we get in startElement-Method
+	private String dmdLogIdDmdSec = null;
+	private String dmdLogId_logicalStructMap = null;
+	private String logId_logicalStructMap = null;
+	private String type = null;
+	private String contentId = null;
+	private String physId_physicalStructMap = null;
+	private String orderLabel = null;
+	private String smLinkTo = null;
+	private String smLinkFrom = null;
 	
+	private MetsRawRecord metsRawRecord = null;
+	private Map<String, DmdSec> dmdSecs = null;
+	private DmdSec dmdSec = null;
+	private List<String> classifications = null;
+	private Participant participant = null;
+	private List<Participant> participants = null;
+	//List<String> classifications = new ArrayList<String>();
+	
+
 	public MetsContentHandler(SolrServer solrServer, String timeStamp, boolean print) {
 		this.solrServer = solrServer;
 		this.timeStamp = timeStamp;
 		this.print = print;
 	}
-	
+
 	@Override
 	public void startDocument() throws SAXException {
-		
+
 	}
-	
-	
+
+
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+
+		// Clear element content for fresh start
+		elementContent = "";
+
+		if (qName.equals("record")) {
+			isRecord = true;
+		}
+
 		
+		if (isRecord) {
+			metsRawRecord = new MetsRawRecord();
+			dmdSecs = new HashMap<String, DmdSec>();
+			
+			if (qName.equals("mets:dmdSec")) {
+				isDmdSec = true;
+				dmdSec = metsRawRecord.new DmdSec();
+				classifications = new ArrayList<String>();
+				participants = new ArrayList<Participant>();
+			}
+
+			if (qName.equals("mets:structMap") && atts.getValue("TYPE") != null && atts.getValue("TYPE").equals("LOGICAL")) {
+				isLogicalStructMap = true;
+			}
+
+			if (qName.equals("mets:structMap") && atts.getValue("TYPE") != null && atts.getValue("TYPE").equals("PHYSICAL")) {
+				isPhysicalStructMap = true;
+			}
+
+			if (qName.equals("mets:structLink")) {
+				isStructLink = true;
+			}
+		}
+
 		
-		// Parse dmdSec sections
-		if (qName.equals("mets:dmdSec")) {
-			System.out.println("Parsing dmdSec");
-			if (qName.equals("mods:classification")) {
-				if (atts.getValue("authority").equals("sswd")) {
-					// Content of node should be the classification
-					System.out.println("Classification: " + elementContent);
+		if (isDmdSec) {
+			
+			if (atts.getValue("ID") != null) {
+				dmdLogIdDmdSec = atts.getValue("ID");
+			}
+
+			if (qName.equals("mods:classification") && atts.getValue("authority") != null && atts.getValue("authority").equals("sswd")) {
+				isClassification = true;
+			}
+
+			if (qName.equals("mods:originInfo") && atts.getValue("eventType") != null && atts.getValue("eventType").equals("publication")) {
+				isOriginInfo = true;
+			}
+
+			if (isOriginInfo) {
+				if (qName.equals("mods:publisher")) {
+					isPublisher = true;
+				}
+
+				if (qName.equals("mods:placeTerm")) {
+					isPlace = true;
+				}
+
+				if (qName.equals("mods:dateIssued")) {
+					isYear = true;
+				}
+			}
+
+			if (qName.equals("mods:relatedItem")) {
+				isRelatedItem = true;
+
+				if (atts.getValue("type") != null && atts.getValue("type").equals("reviewOf")) {
+					isReview = true;
+				}
+			}
+
+			if (isReview) {
+				if (qName.equals("mods:title")) {
+					isReviewedBookTitle = true;
+				}
+			}
+
+			if (qName.equals("mods:part")) {
+				isPart = true;
+			}
+
+			if (isPart) {
+
+				if (atts.getValue("order") != null) {
+					//sortNo = atts.getValue("order");
+					dmdSec.setSortNo(atts.getValue("order"));
+					//System.out.println("sortNo: " + sortNo);
+				}
+
+				if (qName.equals("mods:detail") && atts.getValue("type") != null && atts.getValue("type").equals("issue")) {
+					isIssue = true;
+				}
+
+				if (qName.equals("mods:detail") && atts.getValue("type") != null && atts.getValue("type").equals("volume")) {
+					isVolume = true;
+				}
+
+				if (qName.equals("mods:number")) {
+					isNumber = true;
+				}
+			}
+
+			if (qName.equals("mods:titleInfo") && atts.getLength() == 0) {
+				isTitleInfo = true;
+			}
+
+			if (isTitleInfo) {
+				if (qName.equals("mods:title") && atts.getLength() == 0) {
+					isTitle = true;
+				}
+
+				if (qName.equals("mods:subTitle")) {
+					isSubtitle = true;
+				}
+			}
+
+			if (qName.equals("mods:recordInfo")) {
+				isRecordInfo = true;
+			}
+
+			if (isRecordInfo) {
+				if (qName.equals("mods:recordIdentifier")) {
+					isRecordIdentifier = true;
+					if (atts.getValue("source") != null) {
+						if (atts.getValue("source").equals("gbv-ppn")) {
+							isAcNo = true;
+						}
+						if (atts.getValue("source").equals("ak_identifier")) {
+							isAkIdentifier = true;
+						}
+					}
+				}
+			}
+
+			if (qName.equals("mods:languageTerm")) {
+				isLanguageTerm = true;
+			}
+
+			if (qName.equals("mods:abstract")) {
+				isAbstract = true;
+			}
+
+			if (qName.equals("mods:name")) {
+				isName = true;
+
+				if (atts.getValue("type") != null && atts.getValue("type").equals("personal")) {
+					isPersonalName = true;
+					participant = metsRawRecord.new Participant();
+				}
+
+				if (atts.getValue("type") != null && atts.getValue("type").equals("corporate")) {
+					isCorporateName = true;
+					participant = metsRawRecord.new Participant();
+				}
+			}
+
+			if (isName) {
+				if (qName.equals("mods:roleTerm")) {
+					isRoleTerm = true;
+				}
+
+				if (qName.equals("mods:namePart")) {
+					if (atts.getValue("type") != null && atts.getValue("type").equals("family")) {
+						isFamilyNamePart = true;
+					}
+
+					if (atts.getValue("type") != null && atts.getValue("type").equals("given")) {
+						isGivenNamePart = true;
+					}
+				}
+			}
+
+		}
+
+		if (isLogicalStructMap) {
+			if (qName.equals("mets:div")) {
+				isDiv_logical = true;
+
+				if (atts.getValue("DMDID") != null) {
+					dmdLogId_logicalStructMap = atts.getValue("DMDID");
+					//System.out.println("dmdLogId_logicalStructMap: " + dmdLogId_logicalStructMap);
+				}
+
+				if (atts.getValue("ID") != null) {
+					logId_logicalStructMap = atts.getValue("ID");
+					//System.out.println("logId_logicalStructMap: " + logId_logicalStructMap);
+				}
+
+				if (atts.getValue("TYPE") != null) {
+					type = atts.getValue("TYPE");
+					//System.out.println("type: " + type);
+				}
+
+			}
+		}
+
+		if (isPhysicalStructMap) {
+			if (qName.equals("mets:div")) {
+				isDiv_physical = true;
+
+				if (atts.getValue("TYPE") != null && atts.getValue("TYPE").equals("page")) {
+
+					if (atts.getValue("CONTENTIDS") != null) {
+						contentId = atts.getValue("CONTENTIDS");
+						//System.out.println("contentId: " + contentId);
+					}
+
+					if (atts.getValue("ID") != null) {
+						physId_physicalStructMap = atts.getValue("ID");
+						//System.out.println("physId_physicalStructMap: " + physId_physicalStructMap);
+					}
+
+					if (atts.getValue("ORDERLABEL") != null) {
+						orderLabel = atts.getValue("ORDERLABEL");
+						//System.out.println("orderLabel: " + orderLabel);
+					}
 				}
 			}
 		}
+
+
+		if (isStructLink) {
+			if (qName.equals("mets:smLink")) {
+
+				if (atts.getValue("xlink:to") != null) {
+					smLinkTo = atts.getValue("xlink:to");
+					//System.out.println("smLinkTo: " + smLinkTo);
+				}
+
+				if (atts.getValue("xlink:from") != null) {
+					smLinkFrom = atts.getValue("xlink:from");
+					//System.out.println("smLinkFrom: " + smLinkFrom);
+				}
+			}
+		}
+
+
+
 	}
 
-	
+
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
+
 		
+		
+		if (qName.equals("record")) {
+			metsRawRecord.setDmdSecs(dmdSecs);
+			isRecord = false;
+			System.out.println("\n------------------------------------------------------\n");
+		}
+
+
+		if (isClassification) {
+			classifications.add(elementContent);			
+		}
+
+		if (isPublisher) {
+			dmdSec.setPublisher(elementContent);
+			//System.out.println("publisher: " + publisher);
+		}
+
+		if (isPlace) {
+			dmdSec.setPlace(elementContent);
+			//System.out.println("place: " + place);
+		}
+
+		if (isYear) {
+			dmdSec.setYear(elementContent);
+		}
+
+		if (isVolume && isNumber) {
+			dmdSec.setVolume(elementContent);
+		}
+
+		if (isIssue && isNumber) {
+			dmdSec.setIssueNo(elementContent);
+		}
+
+		if (isTitle && !isReviewedBookTitle) {
+			dmdSec.setTitle(elementContent);
+		}
+
+		/*
+		if (isReviewedBookTitle) {
+			reviewedBookTitle = elementContent;
+			System.out.println("reviewedBookTitle: " + reviewedBookTitle);
+		}
+		 */
+
+		if (isSubtitle) {
+			dmdSec.setSubTitle(elementContent);
+			//System.out.println("subTitle: " + subTitle);
+		}
+
+		if (isPersonalName && isGivenNamePart) {
+			participant.setGivenName(elementContent);
+			
+		}
+
+		if ((isPersonalName || isCorporateName) && isFamilyNamePart) {
+			participant.setFamilyName(elementContent);
+		}
+
+		if (isRoleTerm) {
+			participant.setRole(elementContent);
+		}
+		
+		if (isRecordIdentifier && !isRelatedItem) {
+			if (isAcNo) {
+				dmdSec.setAcNo(elementContent);
+			}
+
+			if (isAkIdentifier) {
+				dmdSec.setAkIdentifier(elementContent);
+			}
+		}
+
+		if (isLanguageTerm) {
+			dmdSec.setLanguageTerm(elementContent);
+		}
+
+		if (isAbstract) {
+			dmdSec.getAbstractTexts().add(elementContent);
+			
+		}
+
+
+
+
+
+		// RESET BOOLEANS TO FALSE:
+		if (qName.equals("mets:dmdSec")) {
+			dmdSec.setClassifications(classifications);
+			dmdSec.setParticipants(participants);
+			dmdSecs.put(dmdLogIdDmdSec, dmdSec);
+			System.out.println(dmdSecs);
+			isDmdSec = false;
+		}
+
+		if (qName.equals("mets:structMap")) {
+			isLogicalStructMap = false;
+			isPhysicalStructMap = false;
+		}
+
+		if (qName.equals("mets:structLink")) {
+			isStructLink = false;
+		}
+
+		if (qName.equals("mods:originInfo")) {
+			isOriginInfo = false;
+		}
+
+		if (qName.equals("mods:classification")) {
+			isClassification = false;
+		}
+
+		if (qName.equals("mods:relatedItem")) {
+			isReview = false;
+			isRelatedItem = false;
+		}
+
+		if (qName.equals("mods:titleInfo")) {
+			isTitleInfo = false;
+		}
+
+		if (qName.equals("mods:title")) {
+			isTitle = false;
+			isReviewedBookTitle = false;
+		}
+
+		if (qName.equals("mods:subTitle")) {
+			isSubtitle = false;
+		}
+
+		if (qName.equals("mods:publisher")) {
+			isPublisher = false;
+		}
+
+		if (qName.equals("mods:placeTerm")) {
+			isPlace = false;
+		}
+
+		if (qName.equals("mods:dateIssued")) {
+			isYear = false;
+		}
+
+		if (qName.equals("mods:recordInfo")) {
+			isRecordInfo = false;
+		}
+
+		if (qName.equals("mods:recordIdentifier")) {
+			isRecordIdentifier = false;
+			isAcNo = false;
+			isAkIdentifier = false;
+		}
+
+		if (qName.equals("mods:part")) {
+			isPart = false;
+		}
+
+		if (qName.equals("mods:detail")) {
+			isIssue = false;
+			isVolume = false;
+		}
+
+		if (qName.equals("mods:number")) {
+			isNumber = false;
+		}
+
+		if (qName.equals("mods:languageTerm")) {
+			isLanguageTerm = false;
+		}
+
+		if (qName.equals("mods:abstract")) {
+			isAbstract = false;
+		}
+
+		if (qName.equals("mods:name")) {
+			participants.add(participant);
+			isName = false;
+			isPersonalName = false;
+			isCorporateName = false;
+		}
+
+		if (qName.equals("mods:roleTerm")) {
+			isRoleTerm = false;
+		}
+
+		if (qName.equals("mods:namePart")) {
+			isFamilyNamePart = false;
+			isGivenNamePart = false;
+		}
+
+		if (qName.equals("mets:div")) {
+			isDiv_logical = false;
+			isDiv_physical = false;
+		}
+
+
 	}
 
-	
+
 	@Override
 	public void endDocument() throws SAXException {	
 	}
-	
-	
+
+
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {	
-		elementContent += new String(ch, start, length);
+		elementContent += new String(ch, start, length).replaceAll("\\s+", " ");
 	}
 
-	
+
 	/**
 	 * Prints the specified text to the console if "print" is true.
 	 * 
@@ -96,11 +579,11 @@ public class MetsContentHandler implements ContentHandler {
 			System.out.print(text);
 		}
 	}
-	
+
 	// Methods of ContentHandler that are not used at the moment
 	@Override
 	public void setDocumentLocator(Locator locator) {}
-	
+
 	@Override
 	public void startPrefixMapping(String prefix, String uri) throws SAXException {}
 
@@ -114,6 +597,8 @@ public class MetsContentHandler implements ContentHandler {
 	public void processingInstruction(String target, String data) throws SAXException {}
 
 	@Override
-	public void skippedEntity(String name) throws SAXException {		}
+	public void skippedEntity(String name) throws SAXException {}
+
+
 
 }
