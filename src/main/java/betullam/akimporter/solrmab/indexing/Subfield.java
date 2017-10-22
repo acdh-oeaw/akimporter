1b31fd23285cfc4b029a07c37146a7552e5515e8
Michael Birkner <michael.birkner@akwien.at>
2017-10-22 10:27:01 +0200

    Overriding equals and hashCode for comparing.

--- src/main/java/betullam/akimporter/solrmab/indexing/Subfield.java
+++ src/main/java/betullam/akimporter/solrmab/indexing/Subfield.java
@@ -32,30 +32,69 @@ public class Subfield {
 	
 	public Subfield() {}
 	
+	
 	public Subfield(String code, String content) {
 		this.code = code;
 		this.content = content;
 	}
-
+	
 	
 	public String getCode() {
 		return code;
 	}
 
+	
 	public void setCode(String code) {
 		this.code = code;
 	}
 
+	
 	public String getContent() {
 		return content;
 	}
 
+	
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subfield other = (Subfield) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		return true;
+	}
+
 	
 	@Override
 	public String toString() {
 		return "Subfield [code=" + code + ", content=" + content + "]";
 	}
+	
 }
